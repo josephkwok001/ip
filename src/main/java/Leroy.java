cbf432583c1736a85f9333267bfbd0637caa54da
Joseph <177612090+josephkwok001@users.noreply.github.com>
2025-08-20 21:12:09 +0800

    Level 2: Completed

--- src/main/java/Leroy.java
+++ src/main/java/Leroy.java
@@ -1,4 +1,5 @@
 import java.util.Scanner;
+import java.util.ArrayList;
 
 public class Leroy {
     public static void main(String[] args) {
@@ -8,20 +9,31 @@ public class Leroy {
         System.out.println("═╦══════════════════════════════════════════════════════════╦═");
 
         Scanner input = new Scanner(System.in);
+        ArrayList<String> list = new ArrayList<>();
 
         while (true) {
             String user_expression = input.nextLine();
 
-            if (user_expression.equalsIgnoreCase("bye")) {
+            if (user_expression.equals("bye")) {
                 System.out.println("═╦══════════════════════════════════════════════════════════╦═");
                 System.out.println("Bye. Hope to see you again soon!");
                 System.out.println("═╦══════════════════════════════════════════════════════════╦═");
                 break;
             }
+            if (user_expression.equals("list")) {
+                System.out.println("═╦══════════════════════════════════════════════════════════╦═");
+                System.out.println("YOUR LIST:");
+                for (int i = 0; i < list.size(); i++) {
+                    System.out.println((i + 1) + ". " + list.get(i));
+                }
+                System.out.println("═╦══════════════════════════════════════════════════════════╦═");
+                continue;
+            }
 
             System.out.println("═╦══════════════════════════════════════════════════════════╦═");
             System.out.println("📢 RECEIVED: " + user_expression);
             System.out.println("═╦══════════════════════════════════════════════════════════╦═");
+            list.add(user_expression);
         }
     }
 }
