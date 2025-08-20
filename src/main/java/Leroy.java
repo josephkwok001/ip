2e6b71dfc9d6ac0d3c032d34c53897accddeedd9
Joseph <177612090+josephkwok001@users.noreply.github.com>
2025-08-20 20:52:11 +0800

    Level-1 completed

--- src/main/java/Leroy.java
+++ src/main/java/Leroy.java
@@ -1,10 +1,27 @@
+import java.util.Scanner;
+
 public class Leroy {
     public static void main(String[] args) {
-        System.out.println("____________________________________________________________\n" +
-                " Hello! I'm Leroy\n" +
-                " What can I do for you?\n" +
-                "____________________________________________________________\n" +
-                " Bye. Hope to see you again soon!\n" +
-                "____________________________________________________________\n");
+        System.out.println("═╦══════════════════════════════════════════════════════════╦═");
+        System.out.println("HEY SOLDIER! I'm LEROY - READY FOR ACTION!");
+        System.out.println(" What can I do for you today?");
+        System.out.println("═╦══════════════════════════════════════════════════════════╦═");
+
+        Scanner input = new Scanner(System.in);
+
+        while (true) {
+            String user_expression = input.nextLine();
+
+            if (user_expression.equalsIgnoreCase("bye")) {
+                System.out.println("═╦══════════════════════════════════════════════════════════╦═");
+                System.out.println("Bye. Hope to see you again soon!");
+                System.out.println("═╦══════════════════════════════════════════════════════════╦═");
+                break;
+            }
+
+            System.out.println("═╦══════════════════════════════════════════════════════════╦═");
+            System.out.println("📢 RECEIVED: " + user_expression);
+            System.out.println("═╦══════════════════════════════════════════════════════════╦═");
+        }
     }
 }
