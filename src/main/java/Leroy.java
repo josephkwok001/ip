f5e09ee54d32562c4d7598ee327b495ccd680e04
Joseph <177612090+josephkwok001@users.noreply.github.com>
2025-08-20 14:12:18 +0800

    Completed Level-0: Basic greeting

---
+++ src/main/java/Leroy.java
@@ -0,0 +1,10 @@
+public class Leroy {
+    public static void main(String[] args) {
+        System.out.println("____________________________________________________________\n" +
+                " Hello! I'm Leroy\n" +
+                " What can I do for you?\n" +
+                "____________________________________________________________\n" +
+                " Bye. Hope to see you again soon!\n" +
+                "____________________________________________________________\n");
+    }
+}
