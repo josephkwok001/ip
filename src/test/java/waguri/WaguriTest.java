42f2b05299600061da021798686979ac99299028
Joseph <177612090+josephkwok001@users.noreply.github.com>
2025-08-26 15:09:35 +0800

    Added Junit Testing

---
+++ src/test/java/waguri/WaguriTest.java
@@ -0,0 +1,18 @@
+package waguri;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+
+public class WaguriTest {
+    @Test
+    public void dummyTest(){
+        assertEquals(2, 2);
+    }
+
+    @Test
+    public void anotherDummyTest(){
+        assertEquals(4, 4);
+    }
+}
